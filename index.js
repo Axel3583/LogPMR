30593f908a82839d4ff727b3245feec7ad482c63
Axel Ibo <axelyvesi2@gmail.com>
2023-10-03 11:49:48 +0200

    LOG-113 middleware pour la gestion des erreurs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const helmet = require("helmet"); // Middleware de sécurité pour Express
 const cors = require("cors"); // Middleware de gestion des CORS (Cross-Origin Resource Sharing)
 const dotenv = require("dotenv"); // Chargement des variables d'environnement
 const mongoose = require("mongoose"); // ODM (Object Data Modeling) pour MongoDB
+const TaskRoute = require("./api/routes/TaskRoute"); // Importation des routes de l'API
 
 dotenv.config(); // Charger les variables d'environnement
 
@@ -26,6 +27,16 @@ server.use(express.urlencoded({ extended: true })); // Middleware pour gérer le
 server.use(express.json()); // Middleware pour gérer les données JSON dans les requêtes
 server.use(helmet()); // Middleware Helmet pour renforcer la sécurité du serveur
 server.use(cors()); // Middleware CORS pour gérer les autorisations d'accès depuis différents domaines
+
+TaskRoute(server); // Utilisation des routes définies dans TaskRoute pour gérer les requêtes liées aux tâches
+
+// Middleware pour la gestion des erreurs (doit être le dernier middleware)
+server.use((err, req, res, next) => {
+  console.error(err.stack); // Affiche les détails de l'erreur dans la console
+  res
+    .status(500)
+    .send({ message: "Internal Server Error", error: err.message }); // Répond avec une erreur HTTP 500 en cas d'erreur
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server is running on http://${hostname}:${port}`); // Démarrage du serveur Express sur l'adresse et le port spécifiés
