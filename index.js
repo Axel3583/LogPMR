e8e2811b3cf8d5e1f13af9fb687e9801915988a3
Axel Ibo <axelyvesi2@gmail.com>
2023-10-03 11:27:20 +0200

    LOG-60 config de moongoDB

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const hostname = '127.0.0.1';
-const port = 3000;
+// Importation des modules nécessaires
+const express = require("express"); // Framework web pour Node.js
+const dotenv = require("dotenv"); // Chargement des variables d'environnement
+const mongoose = require("mongoose"); // ODM (Object Data Modeling) pour MongoDB
 
-const server = express()
+dotenv.config(); // Charger les variables d'environnement
+
+const hostname = '0.0.0.0'; // L'adresse IP sur laquelle le serveur écoutera
+const port = 4000; // Le port sur lequel le serveur écoutera
+
+const server = express(); // Création de l'instance du serveur Express
+
+// Connexion à la base de données MongoDB en utilisant l'URI provenant des variables d'environnement
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB.'); // Connexion réussie à MongoDB
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err); // Erreur de connexion à MongoDB
+    process.exit(1); // Quitter le processus en cas d'erreur de connexion
+  });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server is running on http://${hostname}:${port}`); // Démarrage du serveur Express sur l'adresse et le port spécifiés
 });
