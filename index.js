3d093aa9e5633499fd0781f39f37a6969d68f086
Axel Ibo <axelyvesi2@gmail.com>
2023-10-03 11:44:01 +0200

    LOG-62 middleware

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Importation des modules nécessaires
 const express = require("express"); // Framework web pour Node.js
+const helmet = require("helmet"); // Middleware de sécurité pour Express
+const cors = require("cors"); // Middleware de gestion des CORS (Cross-Origin Resource Sharing)
 const dotenv = require("dotenv"); // Chargement des variables d'environnement
 const mongoose = require("mongoose"); // ODM (Object Data Modeling) pour MongoDB
 
@@ -19,6 +21,11 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('MongoDB connection error:', err); // Erreur de connexion à MongoDB
     process.exit(1); // Quitter le processus en cas d'erreur de connexion
   });
+
+server.use(express.urlencoded({ extended: true })); // Middleware pour gérer les données encodées dans les requêtes
+server.use(express.json()); // Middleware pour gérer les données JSON dans les requêtes
+server.use(helmet()); // Middleware Helmet pour renforcer la sécurité du serveur
+server.use(cors()); // Middleware CORS pour gérer les autorisations d'accès depuis différents domaines
 
 server.listen(port, hostname, () => {
   console.log(`Server is running on http://${hostname}:${port}`); // Démarrage du serveur Express sur l'adresse et le port spécifiés
