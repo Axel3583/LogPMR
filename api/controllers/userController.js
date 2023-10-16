d4982c1dddb78deaa31f6e085e7c22e3651dfd2e
Axel Ibo <axelyvesi2@gmail.com>
2023-10-16 14:40:45 +0200

    Structure MVC pour la gestion des utilisateurs

---
+++ api/controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  // Logique d'inscription ici
+};
+
+exports.login = async (req, res) => {
+  // Logique de connexion ici
+};
