c57a7f46856a6ef8bdf76c8c9bd95b574f938f21
Axel Ibo <axelyvesi2@gmail.com>
2024-03-27 14:28:43 +0100

    Structure of home details informations

--- apps/log-pmr-work-space/src/app/app.component.ts
+++ apps/log-pmr-work-space/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, [FooterComponent]],
   selector: 'logpmr-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
